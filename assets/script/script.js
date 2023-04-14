6dadbf0e6910dd9fc2aadeb52ecb226dc80cc741
faraaryaresta <faraaryaresta@gmail.com>
2023-04-14 21:29:36 +0700

    add bmi calculate

---
+++ assets/script/script.js
@@ -0,0 +1,37 @@
+function calculateBMI() {
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
+
+    let bmi = weight / Math.pow(height / 100, 2);
+
+    let category = "";
+    if (bmi < 18.5) {
+        category = "Underweight";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        category = "Normal Weight";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        category = "Overweight";
+    } else {
+        category = "Obesity";
+    }
+
+    if (height !== '' && weight !== '') {
+        document.getElementById('result').innerHTML = "Your BMI is " + bmi.toFixed(1) + " which means You are " + category;
+        document.getElementById('height_error').innerHTML = '';
+        document.getElementById('weight_error').innerHTML = '';
+    } else {
+        if(height === ''){
+            document.getElementById('height_error').innerHTML = '<i class="fa-regular fa-circle-exclamation"></i> Please provide a valid height';
+            document.getElementById('result').innerHTML = '';
+        }else if (height !== ''){
+            document.getElementById('height_error').innerHTML = '';
+        }
+    
+        if(weight === ''){
+            document.getElementById('weight_error').innerHTML = '<i class="fa-regular fa-circle-exclamation"></i> Please provide a valid weight';
+            document.getElementById('result').innerHTML = '';
+        }else if (weight !== '' ){
+            document.getElementById('weight_error').innerHTML = '';
+        }
+    } 
+};
